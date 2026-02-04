ca5c84da63be74f000645efc785775f07c8357f4
Carlos <carlosvelascoparras@gmail.com>
2026-02-04 16:23:00 +0100

    prueba docs

--- src/backend/discogs_client.py
+++ src/backend/discogs_client.py
@@ -1,8 +1,7 @@
 import discogs_client
+
 from .config import DISCOGS_TOKEN
 
+
 def get_client():
-    return discogs_client.Client(
-        "MiCatalogoVinilos/1.0",
-        user_token=DISCOGS_TOKEN
-    )
+    return discogs_client.Client("MiCatalogoVinilos/1.0", user_token=DISCOGS_TOKEN)
